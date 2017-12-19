789bf4bb29a22ddc32fd729a3311845d2614a854
Justin Briones <justbriones@gmail.com>
2017-12-19 14:59:13 -0800

    added more dependencies, set up mongo connection. Mongoose not quite connected, need to create mongo database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 //require dependencies
 var express = require('express');
+var bodyParser = require('body-parser');
+var expressHandlebars = require('express-handlebars');
+var mongoose = require('mongoose');
 
 //set up port to be host's designated port, or 3000
 var PORT = process.env.PORT || 3000;
@@ -13,8 +16,35 @@ var router = express.Router();
 //designate our public folder as a static directory
 app.use(express.static(__dirname + '/public'));
 
+//connect handlebars to our express app
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+//use bodyparser in app
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 //have every request go through router middleware
 app.use(router);
+
+//if deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var db = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+
+//connect mongoose to our database
+mongoose.createConnection(db, function(error) {
+  //log errors connecting with mongoose
+  if (error) {
+    console.log(error)
+  }
+  else (
+    console.log('mongoose connection is successful')
+  )
+});
 
 //listen on PORT
 app.listen(PORT, function() {
